27d665b969f14c0db6a309afca62da099eb3af7f
sstieber16@googlemail.com <sstieber16@gmail.com>
2017-05-21 17:35:01 +0200

    Mach das mal.

---
+++ RB Game Jam/Assets/Scripts/AP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AP : MonoBehaviour {
+
+	public List<Image> images = new List<Image> ();
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void SetImages(Player player){
+		for (int i = 0; i < images.Count; i++) {
+			if (i < player.ap) {
+				images [i].gameObject.SetActive (true);
+			} else {
+				images [i].gameObject.SetActive (false);
+			}
+		}
+	}
+}
